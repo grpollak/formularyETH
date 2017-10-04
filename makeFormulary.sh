da5e171f6b43f9d40af5a938713fb31a045c5f67
Georg Pollak <pollakg@stud.phys.ethz.ch>
2017-10-04 10:21:18 +0200

    added -e option to sed in order to work correctly

--- makeFormulary.sh
+++ makeFormulary.sh
@@ -4,9 +4,9 @@ formularyName=${1:-formulary}
 mkdir src
 mkdir figures
 
-cp formularyETH/TEMPLATES/formularyTEMPLATE.tex ./$formularyName.tex
+cp formularyETH/TEMPLATES/formularyTEMPLATE.tex ./formulary.tex
 cp formularyETH/TEMPLATES/formularyMacrosTEMPLATE.sty ./formularyMacros.sty
 cp formularyETH/TEMPLATES/README.org ./README.org
-sed -i "s/formulary/$formularyName/" ./README.org
+sed -i -e 's/formulary/$formularyName/' README.org
 cp formularyETH/.gitignore .
 echo Finished
